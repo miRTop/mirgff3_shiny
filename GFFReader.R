dfb3981619aa9088d3dec858296bd4e8cbd1861b
Lorena Pantano <lorena.pantano@gmail.com>
2019-03-29 16:54:04 -0400

    adapt code to get the object done.
    Run each function and understand what is happening
    
    cc @adriancbondia

--- GFFReader.R
+++ GFFReader.R
@@ -23,17 +23,19 @@ coldata_extract <- function(gff_coldata) {
   #Eliminamos el texto superfluo
   coldata2<-coldata %>% str_replace("## COLDATA: ","")
   #Separamos los distintos elementos
-  coldata3<-str_split(coldata2, ",")
+  coldata3<-unlist(str_split(coldata2, ","))
   return(coldata3)
 }
 
 ##Extracción de COLDATA (opcional) de metadata.csv
+# toda la table se necesita para saber que es cada
+# muestra.
+# La primera columna se toma com0 row.names
+# y debe coincidir con `coldata_extract`.
 coldata_extract_csv <- function(csv){
   #Cargamos el archivo de metadatos
-  md_raw <- read_csv(csv)
-  #Seleccionamos los samplenames
-  samplename <- md_raw %>% pull(samplename)
-  return(samplename)
+  md_raw <- read.csv(csv, row.names = 1)
+  md_raw
 }
 
 ##Extracción ROWDATA (GFF sin la columna atribute )
@@ -54,20 +56,30 @@ atributes_extract<-function(gff){
   as_tibble(expresion <-datos1 %>% 
               select(attribute))
   #Separamos los distintos valores de la linea de "attribute" en sus diferentes tipos 
-  expresion2<-datos1 %>% separate(attribute, c("Read","UID","Name","Parent","Variant","Cigar","Expression","Filter","Hits"))
-  return (expresion2)
+  gff_table<-datos1 %>% 
+      mutate(uid = str_extract(attribute,"iso-[0-9]+-\\w+;")) %>% # get the UID to have a unique self-identifier
+      separate_rows(attribute, sep=";") %>%  # separate by row each element in attribute
+      mutate(attribute = trimws(attribute)) %>% # remove leading/tailing spaces
+      separate(attribute, sep = " ", into = c("att", "value")) %>% # separate the values into two columns (UID | iso-22-LVMJ3KW9)
+      spread(att, value) %>% # move name of the attributes to be columns
+      select(-uid) # remove temporal ID
+  
+  # expresion2<-datos1 %>% separate(attribute, c("Read","UID","Name","Parent","Variant","Cigar","Expression","Filter","Hits"), sep = ";")
+  return (gff_table)
 }
 
 ##Extracción de COUNTS
-counts_extract<-function(gff){
+counts_extract<-function(gff, colnames){
   #Aplicamos la función "atributes_extract" para dividir la variable "Attributes"
   datos1<-atributes_extract(gff)
   #Seleccionamos las variables UID y Expression
   as_tibble(uid_exp <-datos1 %>% 
-              select(c(UID,Expression)))
-  #Transformamos el dibble en matriz
-  uid_exp2<-as.matrix(uid_exp)
-  return(uid_exp)
+              select(c(UID, Expression))) %>% 
+      separate(Expression, sep=",", into = colnames, convert = TRUE) %>% 
+      distinct()  %>%  # no duplicados
+      as.data.frame() %>% # because tibble doesn't have row.names
+      column_to_rownames("UID") %>%
+      as.matrix()
 }
 
 ##Función Englobadora. Introducimos un GFF y devuelve un objeto con tres bloques de información: counts, rowdata y coldata
@@ -82,6 +94,24 @@ gff_objects<-function(gff){
   return(newList)
 }
 
+gff = "test/mirtop.gff"
+metadata = "test/metadata.csv"
+colnames<- coldata_extract(gff)
+attributes<-atributes_extract(gff)
+counts<-counts_extract(gff, colnames)
+metadata<-coldata_extract_csv(metadata)
 #Creamos la clase SummarizedExperiment
 #??
-sum_Experiemnt1<-makeSummarizedExperimentFromExpressionSet(gff_objects())
+sum_Experiemnt<-SummarizedExperiment(assays = list(raw = counts),
+                                     colData = metadata,
+                                     rowData = attributes)
+
+
+# Make function to normalize with DESeq2
+# see dds <- DESeq2::DESeqDataSetFromMatrix(counts, metadata, design=~1)
+# see vst <- DESeq2::varianceStabilizingTransformation(dds)
+# add new normalized count data to new slot in the object 
+## sum_Experiemnt@assays[["vst"]] <- assay(vst)
+
+# Make function to PCA
+# see DEGreport::degPCA()
