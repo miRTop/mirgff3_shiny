558359b35b0e4e7fa880436593704b2d82de4b2d
Adrián Corral Bondía <adriancbondia@gmail.com>
2019-04-30 16:05:32 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,46 +1,39 @@
-#Paquetes y librerias
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-#  install.packages("BiocManager")
-#BiocManager::install(c("SummarizedExperiment","DESeq2","DEGreport"), version = "3.8")
-#library(DESeq2)
-#library(tidyverse)
-#library(stringr)
-#library(SummarizedExperiment)
-#library(purrrlyr)
-#library(ggplot2)
-#library(DEGreport)
+#Load all the necessary packages and libraries.
+if (!requireNamespace("BiocManager", quietly = TRUE))
+install.packages("BiocManager")
+BiocManager::install(c("SummarizedExperiment","DESeq2","DEGreport"), version = "3.8")
+library(DESeq2)
+library(tidyverse)
+library(stringr)
+library(SummarizedExperiment)
+library(purrrlyr)
+library(ggplot2)
+library(DEGreport)
 library(shiny)
-
-#Iniciamos el Interfaz de usuario para la aplicación shiny.
 shinyUI(fluidPage(
-  #Título de la aplicación.
-  titlePanel("GFF Reader"),
-  #Distribución general de la interfaz.
+  titlePanel("mirGFF3 Reader"),
   sidebarLayout(
-    #Distribución del panel lateral.
     sidebarPanel(
-      #Input de archivos GFF y CSV.
-      fileInput("file1", "Choose CSV File"),
-      fileInput("file2", "Choose GFF File"),
-      #Linea de separación.
-      hr(),
-      #Selección de columna del archivo de metadatos.
-      selectInput("columna", "Columna CSV:",   choices=colnames),
-      #Texto explicativo.
-      helpText("Choose the CSV column to coloring."),
-      #Separación.
-      tags$hr(),
-      #Cajetilla que registra si se desean normalizar los datos. Desactivada por defecto.
-      checkboxInput(inputId = "noise",label = strong("Correcting for noise"),value = FALSE)
+      #Menú para cargar el arhivo GFF y CSV.
+      fileInput("file1", "Choose CSV File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      fileInput("file2", "Choose GFF File",
+                multiple = FALSE,
+                accept = c(".gff")),
+      #Botón de acción para lanzar la aplicación.
+      actionButton("upload", "Upload Data")
     ),
-    #Distribución del panel central.
+    #Panel central con un sistema de pestañas.
     mainPanel(
-      #Diferentes pestañas que muestran las variables "table" y "pca"
-      tabsetPanel(type = "tabs",
-                  tabPanel("Info", textOutput("info")),
-                  tabPanel("Table", plotOutput("table")),
-                  tabPanel("PCA", plotOutput("pca"))
-      )
+        tabsetPanel(type = "tabs",
+                    #Pestaña con información sobre el SummarizedExperiment
+                    tabPanel("Info", verbatimTextOutput("contenido")),
+                    #Pestaña con la PCA sin colorear
+                    tabPanel("PCA", plotOutput("pca"))
+        )
     )
   )
 ))
