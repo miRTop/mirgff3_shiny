059d5e1dc7ebe7cf3e20ae88cf5851eb2b559cdb
Adrián Corral Bondía <adriancbondia@gmail.com>
2019-05-09 17:06:04 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -9,8 +9,11 @@ library(DEGreport)
 library(shiny)
 library(DT)
 shinyUI(fluidPage(
+  #Titulo de la aplicación
   titlePanel("mirGFF3 Reader"),
+  #Disposición general del interface de la aplicación.
   sidebarLayout(
+    #Disposición del panel lateral.
     sidebarPanel(
       #Menú para cargar el arhivo GFF y CSV.
       fileInput("file1", "Choose CSV File",
@@ -21,16 +24,22 @@ shinyUI(fluidPage(
       fileInput("file2", "Choose GFF File",
                 multiple = FALSE,placeholder = "test/mirtop.gff",
                 accept = c(".gff")),
+      
       #Botón de acción para lanzar la aplicación.
       actionButton("upload", "Upload Data")
     ),
-    #Panel central con un sistema de pestañas.
+    #Disposición del panel central con un sistema de pestañas.
     mainPanel(
         tabsetPanel(type = "tabs",
                     #Pestaña con información sobre el SummarizedExperiment
                     tabPanel("Info", verbatimTextOutput("contenido")),
                     #Pestaña con la PCA sin colorear
-                    tabPanel("PCA", plotOutput("pca")),
+                    tabPanel("PCA", plotOutput("pca"),
+                             #Menu desplegable con las filas de metadata
+                             selectInput("datadrop","metadata", choices = colnames(metadata)),
+                             #Botón de acción para 
+                             actionButton("upload2", "Upload Data")
+                             ),
                     #Pestaña con la tabla de isomeros y un gráfico con los mismos donde podemos seleccionar las filas a destacar.
                     tabPanel("Prueba", fluidRow(column(6, DT::dataTableOutput("tabla1")),
                                                column(6, plotOutput("grafico1"))
