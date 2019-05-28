af48f0d0387805723fc173c70a98e31d48b30a3d
Adrián Corral Bondía <adriancbondia@gmail.com>
2019-05-28 12:52:37 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -9,64 +9,47 @@ library(DEGreport)
 library(shiny)
 library(DT)
 shinyUI(fluidPage(
-  #Añadimos el tema de la aplicación
-  theme = shinytheme("simplex"),
-  #Titulo de la aplicación
-  titlePanel("mirGFF3 Reader"),
-  #Disposición general del interface de la aplicación.
-  sidebarLayout(
-    #Panel lateral.
-    sidebarPanel(
-      #Menú para cargar el archivo mirGFF3 y CSV.
-      fileInput("file1", "Choose CSV File",
-                multiple = FALSE,placeholder = "test/metadata.csv",
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      fileInput("file2", "Choose GFF File",
-                multiple = FALSE,placeholder = "test/mirtop.gff",
-                accept = c(".gff")),
-      
-      #Botón de acción para lanzar la aplicación.
-      actionButton("upload", "Upload Data")
-    ),
-    #Disposición del panel central con un sistema de pestañas.
-    mainPanel(
-        tabsetPanel(type = "tabs",
-                    #Pestaña con información sobre el SummarizedExperiment
-                    tabPanel("Info", verbatimTextOutput("contenido")),
-                    #Pestaña con la PCA sin colorear
-                    tabPanel("PCA", plotOutput("pca"),
-                             #Menu desplegable con las filas de metadata
-                             selectInput("datadrop","metadata", choices = ""),
-                             #Botón de acción para confirmar la elección de la columna de metadata.
-                             actionButton("upload2", "Render Plot")
-                             ),
-                    #Pestaña con una tabla de los datos y la capacidad de seleccionar la fila y obtener los datos de cada isomero.
-                    tabPanel("Selector", DT::dataTableOutput("tabla4"),
-                             verbatimTextOutput("info"),plotOutput("graph")),
-                    #Pestaña con información sobre expresión diferencial
-                    tabPanel("Differential Expression", 
-                             #Casilla activada por defecto que selecciona la normalización o no de los datos
-                             checkboxInput(inputId = "normalize",
-                                           label = strong("Data normalization"),
-                                           value = TRUE),
-                             #Encabezado de DESeqDataSet normalizado (varianceStabilizingTransformation) o no.
-                             verbatimTextOutput("expresion"), 
-                             hr(),
-                             #Botón que permite mostrar los valores de dispersion.
-                             actionButton("upload3", "Dispersion"),
-                             verbatimTextOutput("expresion2"),
-                             hr(),
-                             #Boton que permite mostrar el DEGplot de los 12 primeros isomiR.
-                             actionButton("upload4", "Grafico"),
-                             plotOutput("expresion_plot"),
-                             #Boton que permite mostrar el MA-Plot.
-                             actionButton("upload5", "Grafico2"),
-                             plotOutput("expresion_plot2")
-                             )
-
+    #Tema de diseño
+    theme = shinytheme("united"),
+    #Titulo de la aplicación
+    titlePanel("mirGFF3 Reader"),
+    #Disposición general del interface de la aplicación.
+    sidebarLayout(
+        #Disposición del panel lateral.
+        sidebarPanel(
+            #Menú para cargar el arhivo GFF y CSV.
+            fileInput("file1", "Choose CSV File",
+                      multiple = FALSE,placeholder = "test/metadata.csv",
+                      accept = c("text/csv",
+                                 "text/comma-separated-values,text/plain",
+                                 ".csv")),
+            fileInput("file2", "Choose GFF File",
+                      multiple = FALSE,placeholder = "test/mirtop.gff",
+                      accept = c(".gff")),
+            #Casilla activada por defecto que selecciona la normalización o no de los datos
+            checkboxInput(inputId = "normalize",
+                          label = strong("Data normalization"),
+                          value = TRUE),
+            #Botón de acción para lanzar la aplicación.
+            actionButton("upload", "Upload Data")
+        ),
+        #Disposición del panel central con un sistema de pestañas.
+        mainPanel(
+            tabsetPanel(type = "tabs",
+                        #Pestaña con información sobre el SummarizedExperiment
+                        tabPanel("Info", verbatimTextOutput("contenido")),
+                        #Pestaña con la PCA sin colorear
+                        tabPanel("PCA", plotOutput("pca"),
+                                 #Menu desplegable con las filas de metadata
+                                 selectInput("datadrop","metadata", choices = ""),
+                                 #Botón de acción para 
+                                 actionButton("upload2", "Render plot")
+                        ),
+                        #Pestaña con una tabla de los datos y la capacidad de seleccionar la fila y obtener los datos de cada isomero.
+                        tabPanel("Selector", DT::dataTableOutput("tabla4"),
+                                 verbatimTextOutput("info"),plotOutput("graph"))
+                        
+            )
         )
     )
-  )
 ))
