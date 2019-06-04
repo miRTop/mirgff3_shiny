9ea03ba0ede5e52e6ea28a900e81e4d217474b9e
Adrián Corral Bondía <adriancbondia@gmail.com>
2019-06-04 13:36:02 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -47,17 +47,7 @@ shinyUI(fluidPage(
                         ),
                         #Pestaña con una tabla de los datos y la capacidad de seleccionar la fila y obtener los datos de cada isomero.
                         tabPanel("Selector", DT::dataTableOutput("tabla4"),
-                                 verbatimTextOutput("info"),plotOutput("graph")),
-                         tabPanel("Differential Expression", 
-                                 hr(),
-                                 textInput('formula1', 'Enter design'),
-                                 p('Type the formula for your differential expression analysis.'),
-                                 actionButton("upload3", "Load design"),
-                                 hr(),
-                                 selectInput("datadrop2","Metadata", choices = ""),
-                                 actionButton("upload4", "VST graph"),
-                                 plotOutput("graph2")
-                        )
+                                 verbatimTextOutput("info"),plotOutput("graph"))
                         
             )
         )
